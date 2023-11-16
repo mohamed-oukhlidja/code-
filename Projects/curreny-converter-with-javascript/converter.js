95e46776f30a48cd295da709496232b0585a4faa
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2023-11-16 15:22:48 +0100

    get the currency codes from API

--- Projects/curreny-converter-with-javascript/converter.js
+++ Projects/curreny-converter-with-javascript/converter.js
@@ -1,9 +1,23 @@
 // API PROVIDERS
 const ipdata = {
-    key: "2d9f9061ae1738bb9a263719337f85ff8062692d30f04ee9d3a76231",
-    baseUrl: "https://api.ipdata.co",
-    currency: function (params) {
-      return `${this.baseUrl}/currency?api-key=${this.key}`;
-    },
-  };
-  
+  baseurl: "https://api.ipdata.co",
+  key: "13952c49cd907f6b9a168b62b0b2364ba3eb022e303cd6aec6afb8cd",
+
+  currency: function () {
+    return `${this.baseurl}/currency?api-key=${ipdata.key}`;
+  },
+};
+const exchangeRate = {
+  baseurl: "https://v6.exchangerate-api.com/v6",
+  key: "fe88a3d526b3b880c429025d",
+
+  convert: function (fromCurrenvyCode, toCurrenvyCode, amount) {
+    // THIS API IS NOT FREE ANYMORE
+    // return `${this.baseurl}/convert?from=${fromCurrenvyCode}&to=${toCurrenvyCode}&amount=${amount}&access_key=${this.key}`;
+    return `${this.baseurl}/${this.key}/pair/${fromCurrenvyCode}/${toCurrenvyCode}/${amount}`;
+  },
+
+  list: function () {
+    return `${this.baseurl}/${this.key}/codes`;
+  },
+};
